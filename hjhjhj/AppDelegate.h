6dc3fb37e3721dd2eb07a024743755c2e7f51246
白朕玮 <18622084589@163.com>
2017-11-02 01:24:33 +0800

    no message

---
+++ hjhjhj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hjhjhj
+//
+//  Created by 白朕玮 on 2017/11/2.
+//  Copyright © 2017年 白朕玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hjhjhj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hjhjhj
+//
+//  Created by 白朕玮 on 2017/11/2.
+//  Copyright © 2017年 白朕玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
